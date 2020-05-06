4086f6d3b6b7a02ff742ef158a59f4367192d8de
Aaron Watters <awatters@simonsfoundation.org>
2020-05-06 16:58:12 -0400

    try wrapping describe in async function

--- tests/headless.test.js
+++ tests/headless.test.js
@@ -1,26 +1,30 @@
 
 // these tests use puppeteer and headless chrome
 
-describe("headless browser tests", async () => {
-    it("gets the browser version",  async () => {
-        var version = await browser.version();
-        console.log("browser version: " + version);
-        expect(version).toBeTruthy();
-    });
-    
-    it("gets a page object",  async () => {
-        const page = await browser.newPage();
-        console.log("page: " + page);
-        expect(page).toBeTruthy();
-    });
+var run = async() => {
+    await describe("headless browser tests", async () => {
+        await it("gets the browser version",  async () => {
+            var version = await browser.version();
+            console.log("browser version: " + version);
+            expect(version).toBeTruthy();
+        });
+        
+        await it("gets a page object",  async () => {
+            const page = await browser.newPage();
+            console.log("page: " + page);
+            expect(page).toBeTruthy();
+        });
 
-    it("gets a page title",  async () => {
-        const page = await browser.newPage();
-        const url = "http://127.0.0.1:3000/html/index.html";
-        await page.goto(url);
-        var title = await page.title();
-        console.log("page title is: " + title);
-        expect(title).toBe("Test page");
-    });
+        await it("gets a page title",  async () => {
+            const page = await browser.newPage();
+            const url = "http://127.0.0.1:3000/html/index.html";
+            await page.goto(url);
+            var title = await page.title();
+            console.log("page title is: " + title);
+            expect(title).toBe("Test page");
+        });
 
-})
+    });
+};
+
+run();
